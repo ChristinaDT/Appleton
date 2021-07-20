e7e65df9f59e0cbfbd84200bae48ca3ab8e57fb8
ChristinaDolganiuc <74195190+ChristinaDT@users.noreply.github.com>
2021-07-20 09:08:51 -0500

    HpDesktop

---
+++ src/NewDevScript.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class NewDevScript {
+    public static void main(String[] args) {
+        String[] names={"Paula","Bianca"};
+        System.out.println(Arrays.toString(names));
+    }
+
+}
